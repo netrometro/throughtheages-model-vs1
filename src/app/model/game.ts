99e742600863ef4e424d60f87fa025d3583c3448
netrometro <netrometro@yahoo.com.br>
2019-09-07 11:43:13 -0300

    Planejamento da arquitetura

---
+++ src/app/model/game.ts
@@ -0,0 +1,55 @@
+// Determinar o nome do jogador
+// Determinar o número de jogadores
+// Inicializar as redes neurais com os dnas
+// Determinar ordem dos jogadores
+
+// Embaralhar todos os decks
+
+// SETUP INICAL
+
+
+// FASE SETUP para a primeira rodada: primeiro jogador uma ação, segundo duas etc.
+// FASE AÇÃO comprar cartas
+  // Verifica o limite de cartas na mão que é definido pela quantidade de cubos brancos.
+  // Escolhe a carta (o limite cartas na mão é o limite de cubos brancos)
+  // Verifica se o valor da carta é compatível
+  // Apresenta mensagem que comprou ou que não pode comprar
+  // Volta para AÇÃO caso ainda tenha ações
+  // Caso não tenha mais ações, ou encerre o turno [TURN OFF] inicia a FASE DE PRODUÇÃO
+
+// FASE DE PRODUÇÃO
+  // Jogador seleciona as cartas militares que deseja descartar até o limite (igual aos cubos vermelhos)
+  // Mensagem dizendo o que ganhou de pontos cultura e ciência
+  // Corrupção: seleção de recursos para pagamento de corrupção, indicador de Corrupção no banco azul. Primeiro PEDRAS, Segundo Comiga, terceiro pontos de cultura.
+  // Produção de comida: um cubo azul para cada cubo amarelo nas cartas do tipo agricultura
+  // Pagar comida a população: indicador no banco amarelo. perde cultura para cada ponto não pago
+  // Produção de recursos: um cubo azul para cada cubo amarelo nas cartas do tipo mineração
+  // Compara cartas militares para cada cubo vermelho não utilizado
+  // Retornar todas as ações para carta de governo (4 cubos brancos e 2 vermelhos)
+
+// FASE REPOSIÇÃO DE CARTAS (não acontece na fase A)
+// As cartas iniciais são descartas dependendo no número de jogadores
+// Cartas andam na trilha
+// espaços vagos na trilha são preencidos
+// No final do turno do primeiro jogador na segunda rodada do jogo, o baralho de antiguidade é substituido pelo de Era 1
+// OBS: cartas militares podem aparecer e últimos jogadores da rodada poderão comprar cartas militares.
+
+
+// SETUP para a segunda rodada
+
+
+
+// Repetição infinita
+
+// Usuários podem comprar cartas
+
+
+
+
+
+
+
+
+
+// Verifica as cláusulas de encerramento da partida
+// Repete processo
---
+++ src/app/model/gamestate.interface.ts
@@ -0,0 +1,14 @@
+
+export interface GameState {
+
+  playerName: String;
+
+  limitBuilds: number;
+  limitMilitaryCards: number;
+
+  handPlayer: number[];
+  cubosbrancos: number;
+  cubosvermelhos: number;
+
+
+}
